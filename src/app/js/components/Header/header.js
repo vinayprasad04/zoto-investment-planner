450151ecb9d5bb336df5690c32dc5136db3de197
Anil18022001 <princeaniljha@gmail.com>
2022-11-01 17:20:42 +0530

    Update navbar

--- src/app/js/components/Header/header.js
+++ src/app/js/components/Header/header.js
@@ -1,45 +1,197 @@
-import {React} from 'react';
-import {Navbar, Container, Nav, NavDropdown} from "react-bootstrap";
-import {Link} from "react-router-dom";
+import { React } from 'react';
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+// import { Link } from "react-router-dom";
+import './navbar.css'
+import logo from '../../../../images/logo.png'
+import relaManger from '../../../../images/relaManger.png'
+import clearNoIcon from '../../../../images/clearNoIcon.png'
 
-const Header =()=>{
-    return(
-        <Navbar collapseOnSelect expand="lg">
-            <Container>
-                <Navbar.Brand href="/">Zoto Investing </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+const Header = () => {
 
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Link to="/" className={'nav-link'}>Home</Link>
+    return (
+        <div className="headerContainer">
+            <header
+                className="header
+      
 
+                
+      "
+            >
+                <div className="container group">
+                    <div className="logo">
+                        <a href="javascript:void(0);">
+                            <img src={logo} alt="prosperx" />
+                        </a>
+                    </div>
+                    {/*//logo*/}
+                    <nav className="navigation">
+                        <ul>
+                            <li>
+                                <a href="javascript:void(0);">Products</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0);">Goals</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0);">Community</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0);">Ask a Question</a>
+                            </li>
+                        </ul>
+                    </nav>
+                    {/*//navigation*/}
+                    <div className="headerRight">
+                        <div className="headSearch">
+                            <input type="text" placeholder="Search " />
+                        </div>
+                        <div className="headInbox">
+                            <a href="javascript:void(0);" className="headInboxBtn">
+                                {" "}
+                                <span>2</span>
+                            </a>
+                            <div className="cartPreviewBox inboxPreviewBox">
+                                <div className="cartTopArrow" />
+                                <h3>2 UNREAD</h3>
+                                <article className="group">
+                                    <div className="inboxUserpic">
+                                        <img src={relaManger} />
+                                    </div>
+                                    <div className="inboxNotCont">
+                                        <strong>Nikhil Dua</strong>
+                                        <br />
+                                        <em>
+                                            Hi there thanks for sending request for your Investment. If
+                                            you are considering to final…
+                                        </em>
+                                        <br />
+                                        Yesterday, 09:43pm
+                                    </div>
+                                </article>
+                                <article className="group">
+                                    <div className="inboxUserpic">
+                                        <img src={clearNoIcon} />
+                                    </div>
+                                    <div className="inboxNotCont">
+                                        <strong>PROSPERX Support</strong>
+                                        <br />
+                                        <em>
+                                            Hi there thanks for sending request for your Investment. If
+                                            you are considering to final…
+                                        </em>
+                                        <br />
+                                        12th July
+                                    </div>
+                                </article>
+                                <div className="allMessageLink">
+                                    <a href="javascript:void(0);">All Messages</a>
+                                </div>
+                            </div>
+                            {/*//inboxPreviewBox*/}
+                        </div>
+                        {/*//headInbox*/}
+                        <div className="headNotification">
+                            <a href="javascript:void(0);" className="headNotificationBtn">
+                                {" "}
+                                <span>2</span>
+                            </a>
+                            <div className="cartPreviewBox notifyPreviewBox">
+                                <div className="cartTopArrow" />
+                                <h3>NOTIFICATIONS</h3>
+                                <article className="clearNotify">
+                                    <strong>
+                                        Your documents are approved for Axis Mutual Fund Purchase.{" "}
+                                    </strong>
+                                    <br />
+                                    Yesterday, 09:43pm
+                                </article>
+                                <article className="clearNotify">
+                                    <strong>Example notification text here</strong>
+                                    <br />
+                                    Yesterday, 09:43pm
+                                </article>
+                                <article className="notClearNotify">
+                                    <strong>
+                                        New! Now you can plan &amp; start investing with Goal based
+                                        approach.
+                                    </strong>
+                                    <br />
+                                    18th July, 05:10pm
+                                </article>
+                            </div>
+                            {/*//notifyPreviewBox*/}
+                        </div>
+                        {/*//headNotification*/}
+                        <div className="headCart">
+                            <a href="javascript:void(0);" className="headCartBtn">
+                                {" "}
+                                <span>2</span>
+                            </a>
+                            <div className="cartPreviewBox">
+                                <div className="cartTopArrow" />
+                                <h3>2 Products in your Cart</h3>
+                                <article>
+                                    <strong>Kotak World Gold Standard fund.</strong>
+                                    <br />
+                                    SIP, Monthly, Rs.18,000
+                                </article>
+                                <article>
+                                    <strong>DSP Blackrock Gold Fund</strong>
+                                    <br />
+                                    SIP, Monthly, Rs.18,000
+                                </article>
+                                <div className="veiwCartBtn">
+                                    <a href="javascript:void(0);">View Cart</a>
+                                </div>
+                            </div>
+                            {/*//cartPreviewBox*/}
+                            <div className="cartNotificationBox">
+                                <div className="cartTopArrow" />
+                                <article>
+                                    <p>
+                                        Kotak World Gold Standard fund has been added to your cart.{" "}
+                                    </p>
+                                    <a href="javascript:void(0);">View Cart</a>
+                                </article>
+                            </div>
+                            {/*//cartPreviewBox*/}
+                        </div>
+                        {/*//headCart*/}
+                        <div className="headLogIn">
+                            <a href="javascript:void(0);">
+                                <span>NG</span>
+                                Nitin Garg
+                            </a>
+                            <div className="profileDropBox text-center">
+                                <ul>
+                                    <li>
+                                        <a href="javascript:void(0);">PROFILE</a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0);">ACCOUNT SETTINGS</a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="${pageContext.request.contextPath}/logout"
+                                            id="lgt_loginBtn"
+                                        >
+                                            Logout
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            {/*//cartPreviewBox*/}
+                        </div>
+                        {/*//headLogout*/}
+                    </div>
+                    {/*//headerRight*/}
+                </div>
+                {/*//container*/}
+            </header>
+            {/*//header*/}
+        </div>
 
-                        <Link to="/stock-planner" className={'nav-link'}>Goals</Link>
-                        <Link to="/mutual-fund-planner" className={'nav-link'}>Mutual Fund Planner</Link>
-                        <Link to="/mix-planner" className={'nav-link'}>Mix Planner</Link>
-
-                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <Link to="/counter" className={'nav-link'}>counter</Link>
-                        </NavDropdown>
-                    </Nav>
-                    <Nav>
-                        <Nav.Link href="#deets">More deets</Nav.Link>
-                        <Nav.Link eventKey={2} href="#memes">
-                            Dank memes
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
     )
 }
 
 export default Header;
-
-
-
-
